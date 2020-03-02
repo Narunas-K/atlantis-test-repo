3d0d559553fe1ede87492a41afb776ba5bc10a6c
Narunas-K <narunas.kapocius@gmail.com>
2020-03-02 21:12:41 +0200

    Delete EC2 instance in us-west-2 region which was created in 'create_ec2_instance' branch

--- main.tf
+++ main.tf
@@ -16,38 +16,38 @@ provider "aws" {
 # RESOURCES
 ##################################################################################
 
-resource "aws_default_vpc" "default" {}
+//resource "aws_default_vpc" "default" {}
 
-resource "aws_security_group" "allow_ssh" {
-  name = "allow_ssh"
-  description = "Allow SSH on port 22 inbond"
- vpc_id = "${aws_default_vpc.default.id}"
+//resource "aws_security_group" "allow_ssh" {
+//  name = "allow_ssh"
+//  description = "Allow SSH on port 22 inbond"
+// vpc_id = "${aws_default_vpc.default.id}"
+//
+//  ingress {
+//      from_port = 22
+//     to_port = 22
+//      protocol = "tcp"
+//      cidr_blocks = ["0.0.0.0/0"]
+//  }
+//  egress {
+//      from_port = 0
+//      to_port = 0
+//      protocol = -1
+//      cidr_blocks = ["0.0.0.0/0"]
+//  }
+//}
 
-  ingress {
-      from_port = 22
-     to_port = 22
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-      from_port = 0
-      to_port = 0
-      protocol = -1
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "centos-vm" {
-  ami           = "ami-8b44f2f3"
-  instance_type = "t2.micro"
-  key_name        = "${var.key_name}"
-  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
-}
+//resource "aws_instance" "centos-vm" {
+//  ami           = "ami-8b44f2f3"
+//  instance_type = "t2.micro"
+//  key_name        = "${var.key_name}"
+//  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+//}
 
 ##################################################################################
 # OUTPUT
 ##################################################################################
 
-output "aws_instance_public_dns" {
-    value = "${aws_instance.centos-vm.public_dns}"
-}
+//output "aws_instance_public_dns" {
+//    value = "${aws_instance.centos-vm.public_dns}"
+//}
